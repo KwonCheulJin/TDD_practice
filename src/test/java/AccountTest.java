2eddfe43d4bb03f4eea3c99a9dfd5b0893b80499
charles <chkftm12@gmail.com>
2021-12-24 14:35:41 +0900

    AccountTest, Account migration

--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -1,10 +1,29 @@
+import static org.junit.jupiter.api.Assertions.fail;
+
 import org.junit.jupiter.api.Test;
 
 class AccountTest {
 
     @Test
-    public void testAccount() throws Exception {
-        Account account = new Account();
+    void testAccount() throws Exception {
+        Account account = new Account(10000);
     }
 
+    @Test
+    void testGetBalance() {
+        Account account = new Account(10000);
+        if(account.getBalance() != 10000) {
+            fail("getBalance() =>" + account.getBalance());
+        }
+
+        account = new Account(1000);
+        if(account.getBalance() != 1000) {
+            fail();
+        }
+
+        account = new Account(0);
+        if(account.getBalance() != 0) {
+            fail();
+        }
+    }
 }
