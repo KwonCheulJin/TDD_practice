27323042524070cd4ddf2c7bbb8f4594b7c6e6ab
charles <chkftm12@gmail.com>
2021-12-24 14:59:54 +0900

    account, accountTest complete

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -9,4 +9,12 @@ public class Account {
     public int getBalance() {
         return this.balance;
     }
+
+    public void deposit(int money) {
+        this.balance += money;
+    }
+
+    public void withdraw(int money) {
+        this.balance -= money;
+    }
 }
--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -1,25 +1,40 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
 
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class AccountTest {
 
+    private Account account;
+
+    @BeforeEach
+    public void setup() {
+        account = new Account(10000);
+    }
+
     @Test
     void testAccount() throws Exception {
-        Account account = new Account(10000);
     }
 
     @Test
     void testGetBalance() {
-        Account account = new Account(10000);
         assertEquals(10000, account.getBalance(),"10000원으로 계좌 생성 후 잔고 조회");
 
         account = new Account(1000);
         assertEquals(1000, account.getBalance());
-
         account = new Account(0);
         assertEquals(0, account.getBalance());
+    }
+
+    @Test
+    void testDeposit() {
+        account.deposit(1000);
+        assertEquals(11000, account.getBalance());
+    }
+
+    @Test
+    void testWithdraw(){
+        account.withdraw(1000);
+        assertEquals(9000, account.getBalance());
     }
 }
