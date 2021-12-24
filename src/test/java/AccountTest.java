d927c6670c5344897f336eaca8a77c621b587158
charles <chkftm12@gmail.com>
2021-12-24 11:54:39 +0900

    TDD 실천법과 도구 firts commit

---
+++ src/test/java/AccountTest.java
@@ -0,0 +1,23 @@
+class AccountTest {
+
+    public void testAccount() throws Exception {
+        Account account = new Account();
+
+        if (account == null) {
+            throw new Exception("계좌생성 실패");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        AccountTest test = new AccountTest();
+        try {
+            test.testAccount();
+        } catch (Exception e) {
+            System.out.println("실패(X)");
+            return;
+        }
+        System.out.println("성공(O)");
+    }
+
+}
